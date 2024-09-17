9d7f424ed51e934b0477076434a2b9e73d432b6c
aksri305 <aksri305@gmail.com>
2024-09-17 20:08:27 +0530

    signup updated

--- FRONTEND/src/pages/SignUp.jsx
+++ FRONTEND/src/pages/SignUp.jsx
@@ -173,15 +173,15 @@ const SignUp = () => {
                     disabled={role === "Bidder"}
                   >
                     <option value="">Select Your Bank</option>
-                    <option value="Meezan Bank">Meezan Bank</option>
-                    <option value="UBL">UBL</option>
-                    <option value="HBL">HBL</option>
-                    <option value="Allied Bank">Allied Bank</option>
+                    <option value="STATE BANK OF INDIA">STATE BANK OF INDIA</option>
+                    <option value="HDFC">HDFC</option>
+                    <option value="UNION BANK OF INDIA">UNION BANK OF INDIA</option>
+                    <option value="ICICI">ICICI</option>
                   </select>
                   <input
                     type="text"
                     value={bankAccountNumber}
-                    placeholder="IBAN / IFSC"
+                    placeholder="IFSC"
                     onChange={(e) => setBankAccountNumber(e.target.value)}
                     className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
                     disabled={role === "Bidder"}
@@ -198,7 +198,7 @@ const SignUp = () => {
               </div>
               <div>
                 <label className="text-[16px] text-stone-600 font-semibold">
-                  Easypaisa And Paypal Details
+                  Razorpay And Paypal Details
                 </label>
                 <div className="flex flex-col gap-1 sm:flex-row sm:gap-4">
                   <input
